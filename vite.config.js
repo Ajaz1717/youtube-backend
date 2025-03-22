b3279a1611bebf0d517068eacb97e6b41592e23f
Ajaz1717 <146579700+Ajaz1717@users.noreply.github.com>
2025-03-22 14:24:48 +0530

    chenges in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+    ], // chenges
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build', // Ensure correct output directory
+    } // yahan takk
 });
