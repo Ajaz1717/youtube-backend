c5accc8c585de050bf11306ff66427c03e953700
Ajaz1717 <146579700+Ajaz1717@users.noreply.github.com>
2025-03-25 12:15:41 +0530

    back to stable

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
 
-    protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class, // ✅ TrustProxies Middleware Added
-        // \Illuminate\Http\Middleware\HandleCors::class,
-    ];
+    // protected $middleware = [
+    //     \App\Http\Middleware\TrustProxies::class, // ✅ TrustProxies Middleware Added
+    //     // \Illuminate\Http\Middleware\HandleCors::class,
+    // ];
 
     protected $middlewareAliases = [
         'api.token' => \App\Http\Middleware\ApiTokenMiddleware::class,
