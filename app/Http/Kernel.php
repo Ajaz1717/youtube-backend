d0de844f60658795a46301512de8af5bd79e3d91
Ajaz1717 <146579700+Ajaz1717@users.noreply.github.com>
2025-03-28 20:30:49 +0530

    remove chenges back to stable

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,14 +6,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-
-    // protected $middleware = [
-    //     \App\Http\Middleware\TrustProxies::class, // ✅ TrustProxies Middleware Added
-    //     // \Illuminate\Http\Middleware\HandleCors::class,
-    // ];
-
     protected $middlewareAliases = [
         'api.token' => \App\Http\Middleware\ApiTokenMiddleware::class,
-        // 'trust.proxies' => \App\Http\Middleware\TrustProxies::class
     ];
 }
