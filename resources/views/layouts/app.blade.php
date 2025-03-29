ba2aaa665b1509939ed7222a232bf30489dffd1b
Ajaz1717 <146579700+Ajaz1717@users.noreply.github.com>
2025-03-29 17:19:56 +0530

    rename css file for pro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ secure_asset('build/assets/app-Di-ucleT.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('build/assets/app-JZ-M837x.css') }}">
         <script src="{{ secure_asset('build/assets/app-7sdSkkRW.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
